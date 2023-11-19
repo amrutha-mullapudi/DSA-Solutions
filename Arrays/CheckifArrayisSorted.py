61dd68ea8ee486865645bdf3dad5a29775e62f93
Amrutha <58883217+amrutha-mullapudi@users.noreply.github.com>
2023-11-19 20:56:12 +0530

    Update and rename Leetcode1752_CheckifArrayisSorted.py to CheckifArrayisSorted.py

--- Arrays/CheckifArrayisSorted.py
+++ Arrays/CheckifArrayisSorted.py
@@ -1,4 +1,5 @@
 '''
+LEETCODE - 1752
 Check if array is sorted - https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
 Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
 There may be duplicates in the original array.
