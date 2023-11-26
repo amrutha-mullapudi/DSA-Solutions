164a63f96bbed29910006674f01e6f038047f9ab
Amrutha <58883217+amrutha-mullapudi@users.noreply.github.com>
2023-11-26 11:48:03 +0530

    Update Merge2SortedArraywithDuplicates.py

--- Arrays/Merge2SortedArraywithDuplicates.py
+++ Arrays/Merge2SortedArraywithDuplicates.py
@@ -1,1 +1,17 @@
+'''
+PROBLEM LINK : https://www.codingninjas.com/studio/problems/sorted-array_6613259
+'''
 
+'''
+APPROACH 1 : BRUTE FORCE
+1. Using a SET in Python. We initialize a set, add the elements from both the arrays into it to remove duplicates and then remove sort it again because the 
+elements added into a set are by default not sorted.
+'''
+def sortedArray(a: [int], b: [int]) -> [int]:
+    s=set(a+b)
+    return sorted(s)
+
+'''
+APPROACH 2 : Using IN PLACE REMOVAL OF DUPLICATES and 2-POINTER APPROACH TO MERGE THE NON-DUPLICATE ARRAYS
+1. Remove the duplicates in place for both the arrays.
+MECHANISM : 
